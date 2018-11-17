b9283848885f1db3bb4119644a727b492f0a6b85
MR.Robot 💀 <JCassio@joselsons-macbook-pro-2.local>
2018-11-17 21:41:08 +0000

    Initial Commit

---
+++ TouchID/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  TouchID
+//
+//  Created by MR.Robot 💀 on 17/11/2018.
+//  Copyright © 2018 Joselson Dias. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+    
+    
+    
+    @IBAction func IDAction(_ sender: Any)
+    {
+        let context: LAContext = LAContext()
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+        {
+            context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Please verify your ID") { (wasSuccessful, error) in
+                if wasSuccessful{
+                    print("Success")
+                }
+                
+                else{
+                    let alert = UIAlertController(title: "Error Authenticating", message: "Unable to authenticate", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                        NSLog("The \"OK\" alert occured.")
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+        
+        else{
+            print("Execute another code because this device does not have touch/face ID")
+        }
+    }
+}
+
